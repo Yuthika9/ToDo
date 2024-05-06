9edf30d30b9c420cec2ee10b389bc833a531039a
iamyuthika <pantyashe@gmail.com>
2024-05-07 00:25:17 +0530

    forth commit

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,31 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
 function ToDoList(){
-    const tasks = [
+    const [tasks,setTasks] = useState([
         "Read SpringBoot",
         "Complete assignments",
         "Prepare breakfast",
         "Sleep for 2 hours",
         "Take a shower"
-      ];
+      ]);
   
+     const handleEmptyButtonClick = () => {
+        setTasks([]); // Clear the list of tasks
+      };
 
-    return(
-        <div className = "todo-list">
-            <h2>
-                <ul>
-                    {tasks.map((task,index) => (
-                        <ToDoItem key={index} task={task}/>
-                    ))}
-                    
-                </ul>
-            </h2>
+      return (
+        <div className="todo-list">
+          <h2>To-Do List</h2>
+           { tasks.length === 0 ? 
+           <p>Nothing to do buddy. Sleep!</p>:
+            (<ul>
+              {tasks.map((task, index) => (
+                <li key={index} >{task}</li>
+              ))}
+            </ul>)}
+
+            <button className = "empty-button" onClick={handleEmptyButtonClick}>Empty</button>
         </div>
-    );
+      );
+
+      
 }
 
-function ToDoItem({ task }) {
-    return <li>{task}</li>;
-  }
 
 export default ToDoList;
